6688e5c4a2e3d487ad613f4b5a972301d6e28e6c
Priyanka saw <priyankasaw51@gmail.com>
2025-06-01 16:31:19 +0530

    Update Palindrome.java

--- BasicQuest/Palindrome.java
+++ BasicQuest/Palindrome.java
@@ -7,14 +7,13 @@ public class Palindrome {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter number");
         int num = sc.nextInt();
-
+        
         int rev = 0;
        int  temp = num;
         while (num > 0) {
             int remainder  = num % 10;
             rev = rev * 10 + remainder;
             num = num / 10;
-
         }
         if(temp == rev){
             System.out.println("palindrome");
